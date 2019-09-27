de3bf4ee31b122179ef42ec37b7cdae92a3e5ad0
moshekan <kanoni.moshe@gmail.com>
2019-09-27 13:53:57 +0300

    Added geofilters

--- nodoron/config.py
+++ nodoron/config.py
@@ -8,15 +8,21 @@ SEARCH_URLS = [
 
 GEO_FILTERS = [
     # Tel aviv
-    GeoFilter(32.103963, 34.803351, 0.5),  # Tel Aviv University Train Station
+    GeoFilter(32.103963, 34.803351, 1),  # Tel Aviv University Train Station
     GeoFilter(32.108510, 34.792238, 1.5),  # Ramat Aviv Alef
-    GeoFilter(32.116657, 34.794643, 1.4),  # Neve Avivim
-    GeoFilter(32.123046, 34.798273, 0.5),  # Ramain Aviv gimel
-    GeoFilter(32.094103, 34.792462, 0.7),  # Yehuda Hamakabi
+    GeoFilter(32.116657, 34.794643, 1.8),  # Neve Avivim
+    GeoFilter(32.123046, 34.798273, 1.5),  # Ramat Aviv gimel
+    GeoFilter(32.094103, 34.792462, 1),  # Yehuda Hamakabi
     # Herzliya
     GeoFilter(32.155013, 34.838615, 0.5),  # Neve Amirim
-    GeoFilter(32.153692, 34.842297, 0.5),  # Ben Gurion Road South
-    GeoFilter(32.157201, 34.842492, 0.3),  # Ben Gurion Road North
+    GeoFilter(32.153692, 34.842297, 0.8),  # Ben Gurion Road South
+    # Ben Gurion Road North
+    GeoFilter(32.157201, 34.842492, 0.8),
+    GeoFilter(32.152358, 34.841837, 0.5),
+    GeoFilter(32.153196, 34.842121, 1),
+    GeoFilter(32.160099, 34.841955, 0.8),
+    GeoFilter(32.162815, 34.842624, 0.8),
+
 
 
 
